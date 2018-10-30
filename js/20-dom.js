195ae5db4fe00f58e0de3d08c1b061662beb0488
saiichigo <guillermo.bulo@gmail.com>
2018-10-29 22:23:37 -0400

    2 video del DOM (stanbay)

--- js/20-dom.js
+++ js/20-dom.js
@@ -9,9 +9,22 @@
 
 // console.log(prueba)
 
-
+// CONSEGUIR ELEMENTO POR EL ID
 var prueba = document.querySelector("#contenedor_master")
- prueba.innerHTML ="CAMBIO DE NOMBRE, PRUEBA"
+var todoslosdiv
+var contendo_div
+prueba.innerHTML ="CAMBIO DE NOMBRE, PRUEBA"
 prueba.style.background = 'green'
+prueba.style.padding = '20px'
+prueba.style.color = 'white'
+prueba.style.className = 'hola hola2'
 
-console.log(prueba)
+// console.log(prueba)
+
+//CONSEGUI ELEMENTO POR ETIQUETA
+todoslosdiv = document.getElementsByTagName('div')
+contendo_div = todoslosdiv[2].textContent	;
+console.log(contendo_div)
+
+//continuar segundo video min 6 exacto
+//CONSEGUIR ELEMENTO POR SU CLASE CSS
